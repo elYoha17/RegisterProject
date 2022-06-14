ff8ce50d424aae7e509c297c68221266ee6f1e0e
Yohanan Miteo <yohananmiteo17@gmail.com>
2022-06-14 23:11:05 +0200

    Add relationships on models

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Register;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Agent extends Model
 {
     use HasFactory;
+
+    public function registers(): BelongsToMany
+    {
+        return $this->belongsToMany(Register::class);
+    }
 }
--- app/Models/Register.php
+++ app/Models/Register.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Agent;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Register extends Model
 {
     use HasFactory;
+
+    public function agents(): BelongsToMany
+    {
+        return $this->belongsToMany(Agent::class);
+    }
 }
